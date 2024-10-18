e8775b7307afdaeccd6a5950e47db9f47dfc8c5d
Anita <anitakosuri8@gmail.com>
2024-10-18 19:49:06 +0530

    String Concepts

---
+++ src/oct/ex_18102024_Strings/Lab147_String_Immutable.java
@@ -0,0 +1,12 @@
+package oct.ex_18102024_Strings;
+
+public class Lab147_String_Immutable {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str3 = "Hello";
+        String str2 = str1.concat(" World");
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
--- src/tasks/task_04102024/PersonClass.java
+++
@@ -1,4 +0,0 @@
-package tasks.task_04102024;
-
-public class Task2 {
-}
