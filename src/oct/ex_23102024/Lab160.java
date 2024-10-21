16c7edf621ab27df3cbd71f61a294ad57c7294d9
Anita <anitakosuri8@gmail.com>
2024-10-21 14:42:52 +0530

    hello

--- src/oct/ex_23102024/Lab160.java
+++ src/oct/ex_23102024/Lab160.java
@@ -2,6 +2,6 @@ package oct.ex_23102024;
 
 public class Lab160 {
     public static void main(String[] args) {
-        System.out.println("Hello Anita!");
+        System.out.println("Hello !!");
     }
 }
